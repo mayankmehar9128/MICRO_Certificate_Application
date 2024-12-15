2e8124a93ba478c69a95da7c1e0ffca661e02b55
MayankMehar <mayank.mehar@yusata.com>
2024-12-15 22:58:59 +0530

    commiting for issue

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -1,21 +1,21 @@
-// import { useState } from 'react'
-// import { assets } from '../../assets/asset'
-// import AdminLoginForm from '@/widgets/AdminLoginForm'
+import { useState } from 'react'
+import { assets } from '../../assets/asset'
+import AdminLoginForm from '@/widgets/AdminLoginForm'
 
-// function AdminLogin() {
+function AdminLogin() {
 
-//   return (
-//     <main className="p-5 m-auto max-w-7xl flex justify-center">
-//       <div className='flex align-middle items-center justify-center gap-28'>
-//         <div className=''>
-//           <AdminLoginForm />
-//         </div>
-//         <div className='hidden sm:block'>
-//           <img src={assets.image31} width={400}/>
-//         </div>
-//       </div>
-//     </main>
-//   )
-// }
+  return (
+    <main className="p-5 m-auto max-w-7xl flex justify-center">
+      <div className='flex align-middle items-center justify-center gap-28'>
+        <div className=''>
+          <AdminLoginForm />
+        </div>
+        <div className='hidden sm:block'>
+          <img src={assets.image31} width={400}/>
+        </div>
+      </div>
+    </main>
+  )
+}
 
-// export default AdminLogin
+export default AdminLogin
